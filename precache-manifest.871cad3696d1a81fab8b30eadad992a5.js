127e9113c5e5ee13c279a7bc13f2e81241a28255
RoshRoslin <RoshRoslin@gmail.com>
2021-05-16 14:37:53 -0700

    Updates

---
+++ precache-manifest.871cad3696d1a81fab8b30eadad992a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05b4355af02c2bc40f1abd31c7cbf7a",
+    "url": "/react-pagination-project/index.html"
+  },
+  {
+    "revision": "42babeda74f25514a8bd",
+    "url": "/react-pagination-project/static/css/main.980c564c.chunk.css"
+  },
+  {
+    "revision": "9a98d25a3e2299b86834",
+    "url": "/react-pagination-project/static/js/2.4fe1a3ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pagination-project/static/js/2.4fe1a3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42babeda74f25514a8bd",
+    "url": "/react-pagination-project/static/js/main.309f6333.chunk.js"
+  },
+  {
+    "revision": "56ce65fe174fb9d2a1d9",
+    "url": "/react-pagination-project/static/js/runtime-main.f3fced86.js"
+  }
+]);
